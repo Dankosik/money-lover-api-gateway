17cfdc26cb141966822a7374d1b905e299a45e37
dankos <daniil.koryto@gmail.com>
2021-10-26 02:05:36 +1000

    optimized imports

--- src/main/java/com/example/moneyloverapigateway/MoneyLoverApiGatewayApplication.java
+++ src/main/java/com/example/moneyloverapigateway/MoneyLoverApiGatewayApplication.java
@@ -1,12 +1,8 @@
 package com.example.moneyloverapigateway;
 
-import feign.Retryer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.loadbalancer.core.ServiceInstanceListSupplier;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -15,7 +11,5 @@ public class MoneyLoverApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(MoneyLoverApiGatewayApplication.class, args);
     }
-
-
 
 }
